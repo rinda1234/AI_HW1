7d9792feea9abbdb4b50d903120361d94143720b
rinda1234 <daebok6962@gmail.com>
2026-03-20 17:41:28 +0900

    Add comments eightpuzzle.py

--- src/eightpuzzle.py
+++ src/eightpuzzle.py
@@ -2,14 +2,14 @@ import random
 
 from search import SearchProblem
 
-
+# 현재 퍼즐 상태 하나를 나타내는 클래스
 class EightPuzzleState:
     """
     This class defines the mechanics of the puzzle itself.
     The task of recasting this puzzle as a search problem is left to the EightPuzzleSearchProblem class.
     """
-
-    def __init__(self, numbers):
+# 퍼즐 상태를 처음 만드는 생성자, numbers는 길이 9짜리 리스트이고 0~8 숫자가 들어있음. 
+    def __init__(self, numbers): # 멤버변수는 여기서 정의되어야함. 밖에서 정의된거는 정적변수처럼 쓰는 듯.
         """
           Constructs a new eight puzzle from an ordering of numbers.
 
@@ -31,17 +31,17 @@ class EightPuzzleState:
         The configuration of the puzzle is stored in a 2-dimensional
         list (a list of lists) 'cells'.
         """
-        self.cells = []
-        numbers = numbers[:]
-        numbers.reverse()
+        self.cells = [] #2차원 배열로 퍼즐 저장 
+        numbers = numbers[:] #원본 리스트 복사
+        numbers.reverse() # 뒤집은 다음 pop()으로 앞에서 부터 꺼낸 것처럼 쓰려고, pop()은 제일 뒤 원소부터 꺼냄. 
         for row in range(3):
             self.cells.append([])
             for col in range(3):
-                self.cells[row].append(numbers.pop())
+                self.cells[row].append(numbers.pop()) # 한행씩 채움. 
                 if self.cells[row][col] == 0:
-                    self.blankLocation = row, col
-
-    def isGoal(self):
+                    self.blankLocation = row, col #0이 있는 위치를 blankLocation에 저장
+    # 현재 퍼즐이 목표상태에 있는지 확인하는 함수. 
+    def isGoal(self): # 클래스 매서드에 self 가 들어가는 이유는 파이썬 클래스 매서드는 그 객체 자신을 같이 받아야 하기 때문에.
         """
           Checks to see if the puzzle is in its goal state.
 
@@ -52,13 +52,14 @@ class EightPuzzleState:
             -------------
             | 7 | 8 | 0 |
             -------------
-
+# 현재 목표 항태
         >>> EightPuzzleState([1, 2, 3, 4, 5, 6, 7, 8, 0]).isGoal()
         True
 
         >>> EightPuzzleState([1, 0, 2, 3, 4, 5, 6, 7, 8]).isGoal()
         False
-        """
+        """ 
+        # 퍼즐판 순서대로 검사.
         current = 1
         for row in range(3):
             for col in range(3):
@@ -66,7 +67,7 @@ class EightPuzzleState:
                     return False
                 current += 1
         return True
-
+# 현재 빈칸에서 어떤 방향으로 움직일 수 있는지 반환하는 함수..
     def legalMoves(self):
         """
           Returns a list of legal moves from the current state.
@@ -88,7 +89,7 @@ class EightPuzzleState:
         if col != 2:
             moves.append('right')
         return moves
-
+# 현재상태에서 move를 적용했을때의 새로운 퍼즐 상태, 새객체를 반환함. 
     def result(self, move):
         """
         Returns a new eightPuzzle with the current state and blankLocation
@@ -116,16 +117,16 @@ class EightPuzzleState:
             newcol = col + 1
         else:
             raise Exception("Illegal Move")
-
+        # 빈퍼즐판을 하나 만들고 현재 퍼즐판을 깊은복사해서 넣음. 
         newPuzzle = EightPuzzleState([0, 0, 0, 0, 0, 0, 0, 0, 0])
         newPuzzle.cells = [values[:] for values in self.cells]
-
+        # 현재 빈칸위치와 이동할 위치의 숫자를 서로 바꿈. 
         newPuzzle.cells[row][col] = self.cells[newrow][newcol]
         newPuzzle.cells[newrow][newcol] = self.cells[row][col]
         newPuzzle.blankLocation = newrow, newcol
 
         return newPuzzle
-
+# 두퍼즐 상태가 같은지 비교하는 함수
     def __eq__(self, other):
         """
             Overloads '==' such that two eightPuzzles with the same configuration
@@ -136,13 +137,13 @@ class EightPuzzleState:
           True
         """
         for row in range(3):
-            if self.cells[row] != other.cells[row]:
+            if self.cells[row] != other.cells[row]: # 각 행이 전부 같으면 같은 상태로 판단 
                 return False
         return True
-
+# 상태릃 set이나 dict의 key로 쓰기 위해 필요. 탐색에서 방문한 상태를 저장할때 사용.set: 값자체만 저장(방문했는지 체크), dict: 값과 key를 저장.
     def __hash__(self):
         return hash(str(self.cells))
-
+# 퍼즐을 예쁘게 출력하기 위한 함수. 
     def __str__(self):
         lines = []
         horizontal_line = ('-' * 13)
@@ -157,22 +158,22 @@ class EightPuzzleState:
             lines.append(horizontal_line)
         return '\n'.join(lines)
 
-
+# 이클래스는 탐색 문제로서의 정의를 담고있음. 
 class EightPuzzleSearchProblem(SearchProblem):
     """
       Implementation of a SearchProblem for the EightPuzzle domain
       Each state is represented by an instance of an eightPuzzle.
     """
-
+# 초기 퍼즐상태를 저장.
     def __init__(self, puzzle):
         self.puzzle = puzzle
-
+# 탐색 시작 상태를 반환, 처음 퍼즐판을 넘겨줌. 
     def getStartState(self):
         return self.puzzle
-
+# 주어진 상태가 목표인지 검사
     def isGoalState(self, state):
         return state.isGoal()
-
+# 현재 상태에서 갈 수 있는 다음 상태들 전부 반환, 예를 들어 가능한 이동이 ['up', 'left']면 up 했을때와 left 했을때 새 상태를 각각 만들어 저장함. 
     def getSuccessors(self, state): # type: ignore
         """
           Returns list of (successor, action, stepCost) pairs where
@@ -183,7 +184,7 @@ class EightPuzzleSearchProblem(SearchProblem):
         for a in state.legalMoves():
             succ.append((state.result(a), a, 1))
         return succ
-
+# 행동 리스트 전체 비용을 계산. 모든 행동비용이 1이므로, 총비용 = 행동 개수.
     def getCostOfActions(self, actions): # type: ignore
         """
          actions: A list of actions to take
@@ -193,7 +194,7 @@ class EightPuzzleSearchProblem(SearchProblem):
         """
         return len(actions)
 
-
+# 랜덤한 퍼즐을 생성하는 함수
 def createRandomEightPuzzle(moves=100):
     """
       moves: number of random moves to apply
