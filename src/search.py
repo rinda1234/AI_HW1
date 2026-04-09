4d78b5ecfb4bf68a2b5c04c928d0bbba0a70eac6
rinda1234 <daebok6962@gmail.com>
2026-04-09 15:46:05 +0900

    update UCS

--- src/search.py
+++ src/search.py
@@ -22,7 +22,8 @@ class SearchProblem:
 
     You do not need to change anything in this class, ever.
     """
-# 시작상태반환
+    
+# 시작상태반환, 
     def getStartState(self): 
         """
         Returns the start state for the search problem.
@@ -153,7 +154,7 @@ def breadth_first_search(problem):
 
     return []
 
-
+#getSuccessors 현재 상태에서 갈 수 있는 다음 상태들 가져옴. successor는 (다음 상태, 그 상태로 가기 위한 행동, 그 행동의 비용) 튜플. successor[0]은 다음 상태, successor[1]은 그 상태로 가기 위한 행동, successor[2]는 그 행동의 비용.
 def uniform_cost_search(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
@@ -166,32 +167,38 @@ def uniform_cost_search(problem):
 # 5. 각 자식 노드의 경로 비용 계산 후 frontier에 삽입, frontier가 비어 있을 때까지 탐색 반복.
 # 모든 간선 비용이 동일한 경우 BFS와 동일한 구조로 동작
 # 중복체크? 현재 상태가 이미 탐색된 상태인지 확인해야하나?
-# 
+
     start = problem.getStartState() 
     node = [(start, "", 0)] 
     frontier = []
     heapq.heappush(frontier, (0, 0, node)) # 비용이 가장 낮은 노드가 먼저 나오도록 우선순위 큐에 추가. (비용, 노드) 튜플로 저장.
-    frontier_states = {start} # 상태 리스트 ex) 123456780  , frontier에 있는 노드들의 상태 집합. set으로 저장해서 빠르게 탐색했는지 확인할 수 있게 함. ucs에선 frontier에 같은 상태가 여러개 들어갈 수 있음. 그러므로 처리 x , 지우기
+   # 현재 상태의 비용보다 작은 비용이 frontier에 있는지 확인하고 있으면 frontier에 노드를 넣으면 안됨. 
+    # fontier에 넣은 상태를 map으로 관리. key: 상태, value: 그 상태까지의 비용. 이 map에 있는 value가 현재 frontier에 넣으려는 노드보다 작을경우 넣지 않기, 
+    # 어차피 explored에 들어가면 이미 탐색된 상태이므로 frintier에 넣지 않음. '
+    # 파이썬에선 dict가 map역할. 
+    # 파이썬에서 언패킹 할때 정보 버리기: _로 받음. 
+    frontier_states = {start: 0}
     explored = set() 
     count = 0 # while 문 안에서 count = 0을 하게 되면 다른 상태에서 파생된 두 상태를 비교할때 서로 같은길이일 경우 우선순위를 비교못함. 
 
     while frontier:
-        node = heapq.heappop(frontier)[2] # 후보에서 비용이 가장 낮은 노드 제거. heapq.heappop(frontier)는 (비용, 노드) 튜플을 반환하므로, [1]로 노드만 가져옴.
+        cost, _, node = heapq.heappop(frontier) # 후보에서 비용이 가장 낮은 노드 제거. 
         state = node[-1][0] 
-        frontier_states.discard(state) 
 
+        # pop 된 순간 그 노드로 가는 경로의 최적해임.
+        
         if problem.isGoalState(state): 
             return [x[1] for x in node][1:]
         
         if state not in explored: 
-            explored.add(state) 
+            explored.add(state) # 현재 상태를 탐색한 상태 집합에 추가
             for successor in problem.getSuccessors(state):
-                if successor[0] not in explored and successor[0] not in frontier_states:
+                if successor[0] not in explored and successor[0] in frontier_states and frontier_states[successor[0]] > cost + successor[2]: # 다음 상태가 아직 탐색되지 않았고 후보에도 없다면:
                     parent = node[:]
                     parent.append(successor)
                     # 매번 길이를 다시 구할 필요 없음 oldcost+ 1하면 됨. 
                     heapq.heappush(frontier, (problem.getCostOfActions([x[1] for x in parent][1:]), count, parent)) # 다음 노드를 우선순위 큐에 추가. 비용은 현재 노드까지의 행동 리스트의 총 비용으로 계산. [x[1] for x in parent][1:]는 parent에서 행동 부분만 추출해서 리스트로 만들고, 첫 번째 요소(시작 상태의 행동)를 제외한 나머지를 반환. problem.getCostOfActions(...)는 그 행동 리스트의 총 비용을 계산.
-                    frontier_states.add(successor[0])
+                    frontier_states[successor[0]] = cost + successor[2]
                     count += 1
 
         
